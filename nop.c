c6b31ef74a9c431024a025ddae8c12a9440c0dd1
Joan254 <joanotieno1998@gmail.com>
2023-09-20 13:29:23 +0000

    Updated

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn't do anythinhg
+ *
+ * @head: head of the linked list
+ * @line_num: line number
+ * Return: void
+ */
+void _nop(stack_t **head, unsigned int line_num)
+{
+	(void)head;
+	(void)line_num;
+}
